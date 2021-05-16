256de875cadc9552e95ad961563eb42e949210b3
Federico Dacal <federicodacal@gmail.com>
2021-05-15 22:03:32 -0300

    TP 2 ABM

--- TP_2_ABM/src/datawarehouse.h
+++
@@ -1,8 +0,0 @@
-#include "ArrayEmployees.h"
-
-#ifndef DATAWAREHOUSE_H_INCLUDED
-#define DATAWAREHOUSE_H_INCLUDED
-
-int hcodeEmployees(Employee list[], int len, int qty, int* pId);
-
-#endif // DATAWAREHOUSE_H_INCLUDED
